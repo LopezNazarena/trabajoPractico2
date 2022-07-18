22b44a874569c6d719d7b7366575754d3d0f8170
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-07-17 21:31:36 -0300

    resolucion del ejercicio 1 del practico 2

---
+++ js/practico2.js
@@ -0,0 +1,21 @@
+// ejercicio 1
+// let edad = prompt("Indique su edad")
+
+// if(edad >= 18){
+//     document.write("Puede conducir")
+// }else{
+//     document.write("No tiene edad para conducir")
+
+// }
+
+// ejercicio 2
+
+let nota = prompt("Ingrese un numero");
+
+switch(nota){
+    case `0`:
+        document.write(
+         "Muy insuficiente"
+        );
+        break;
+}
