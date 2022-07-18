5c75cf1b89588a1c7a49c70d6f19cebab66926aa
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-07-18 16:16:15 -0300

    agregue el ejercicio 2

--- js/practico2.js
+++ js/practico2.js
@@ -12,10 +12,41 @@
 
 let nota = prompt("Ingrese un numero");
 
-switch(nota){
-    case `0`:
-        document.write(
-         "Muy insuficiente"
-        );
-        break;
+switch (nota) {
+  case `0`:
+    document.write("Muy insuficiente");
+    break;
+  case `1`:
+    document.write("Muy insuficiente");
+    break;
+  case `2`:
+    document.write("Muy insuficiente");
+    break;
+  case `3`:
+    document.write("Insuficiente");
+    break;
+  case `4`:
+    document.write("Insuficiente");
+    break;
+  case `5`:
+    document.write("Suficiente");
+    break;
+  case `6`:
+    document.write("Suficiente");
+    break;
+  case `7`:
+    document.write("Bien");
+    break;
+  case `8`:
+    document.write("Notable");
+    break;
+  case `9`:
+    document.write("Notable");
+    break;
+  case `10`:
+    document.write("Sobresaliente");
+    break;
+  default:
+    document.write("Numero erroneo");
+    alert("Introduce un numero valido")
 }
